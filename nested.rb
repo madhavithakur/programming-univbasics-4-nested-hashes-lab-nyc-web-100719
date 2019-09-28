bafedc011463ef089cb3a0ceecec89b3a51f6fb3
Madhavi Thakur <madhavidadhwal91@gmail.com>
2019-09-28 14:53:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -123,9 +123,10 @@ def adding_to_dennis
         :dennis_ritchie => {
           :known_for => "Unix",
          # :languages => ["C", "Assembly"]
+         :languages => ["C"]
         }
      }
-    #programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+    programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
     
   programmer_hash
 end
